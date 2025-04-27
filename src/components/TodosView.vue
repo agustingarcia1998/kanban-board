6cccb9c406a71686e1621accf811e829f7f101e2
agustin <agustingarcia280998@gmail.com>
2025-04-27 17:20:22 -0300

    first commit

---
+++ src/components/TodosView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { reactive } from 'vue'
+let boards = reactive([
+    {
+        id: crypto.randomUUID(),
+        name: 'tablero 1',
+        items: [
+            { 
+                id: crypto.randomUUID(),
+                title: 'Características de archivos'
+            },
+            { 
+                id: crypto.randomUUID(),
+                title: 'Resolver bug'
+            },
+               
+        ]
+    },
+    {
+        id: crypto.randomUUID(),
+        name: 'tablero 2',
+        items: [
+            { 
+                id: crypto.randomUUID(),
+                title: 'Enviar reporte'
+            },
+            { 
+                id: crypto.randomUUID(),
+                title: 'Revisión de código'	
+            },
+               
+        ]
+    }
+])
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="#">Create board</a></li>
+        </ul>
+    </nav>
+    
+    <div class="boards-container">
+        <div class="boards">
+            <div class="board" v-for="board in boards" :key="board.id">
+                <div>{{ board.name }}</div>
+                <div class="items">
+                    <div class="item" v-for="item in board.items" :key="item.id">
+                        {{ item.title }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
